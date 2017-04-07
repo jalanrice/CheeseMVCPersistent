72da79c838c9bde943fd8302124cdd2b770c51ef
Jeffrey Rice <jalanrice@gmail.com>
2017-04-06 20:45:36 -0500

    category controller and view added. viewmodel required next

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using CheeseMVC.Data;
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IList<CheeseCategory> categories = context.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+
+    
+}
--- src/CheeseMVC/Controllers/CheeseController.cs
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -72,5 +72,7 @@ namespace CheeseMVC.Controllers
 
             return Redirect("/");
         }
+
+
     }
 }
